8467ceebf1fd940a4c046ad80e7a894a3ff57373
Sana8367 <sanazahera45@gmail.com>
2026-02-23 22:58:08 +0530

    Updated main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,10 @@ import App from "./App";
 import { AuthProvider } from "./auth/AuthContext";
 import "./index.css";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
   </React.StrictMode>
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 8b86cb27bdedb488488f98b6633d1d75248945a2
